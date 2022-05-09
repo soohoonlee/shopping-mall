22f0ab279f3bf9e7fc0a43fb245077be56e909d8
soohoonlee <jjinimania@gmail.com>
2022-05-09 23:53:13 +0900

    Initial commit//routing

---
+++ src/app.tsx
@@ -0,0 +1,21 @@
+import {QueryClientProvider} from "react-query";
+import {useRoutes} from "react-router-dom";
+import {routes} from "./routes";
+import {getClient} from "./queryClient";
+import {ReactQueryDevtools} from "react-query/devtools";
+import Gnb from "./components/gnb";
+
+const App = () => {
+    const elem = useRoutes(routes)
+    const queryClient = getClient();
+
+    return (
+        <QueryClientProvider client={queryClient}>
+            <Gnb/>
+            {elem}
+            <ReactQueryDevtools initialIsOpen={false}/>
+        </QueryClientProvider>
+    )
+}
+
+export default App
---
+++ src/pages/products/index.tsx
@@ -0,0 +1,25 @@
+import {useQuery} from "react-query";
+import {fetcher, QueryKeys} from "../../queryClient";
+import {Product} from "../../../types";
+import ProductItem from "../../components/product/item";
+
+const ProductList = () => {
+    const {data} = useQuery<Product[]>(QueryKeys.PRODUCTS, () => fetcher({
+        method: 'GET',
+        path: '/products'
+    }))
+    console.log(data)
+
+    return (
+        <div>
+            <h2>상품목록</h2>
+            <ul className="products">
+                {data?.map(product => (
+                    <ProductItem {...product} key={product.id}/>
+                ))}
+            </ul>
+        </div>
+    )
+}
+
+export default ProductList
